4c8da32564eb44bd82424443707802db6bb40aa2
Oleksandr <saharek123123@gmail.com>
2022-11-20 17:29:59 +0200

    modified StudentUtils (deleted @builder)

--- src/main/java/com/example/demo/StudentUtils.java
+++ src/main/java/com/example/demo/StudentUtils.java
@@ -2,11 +2,9 @@ package com.example.demo;
 
 import java.util.UUID;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class StudentUtils {
 	public static String generateId() {
 		return UUID.randomUUID().toString();
